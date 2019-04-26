ed3bf9fb65abf4eae7436d358fe1bac4afa67e12
Nicholas Wang <nicholaswang2000@berkeley.edu>
2019-04-25 18:15:25 -0700

    Merged everything alright

--- iClassAuth/AppDelegate.swift
+++ iClassAuth/AppDelegate.swift
@@ -8,15 +8,27 @@
 
 import UIKit
 import Firebase
+import CoreLocation //colin
+import UserNotifications //colin
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    static let geoCoder = CLGeocoder()
+    let center = UNUserNotificationCenter.current() //colin
+//    let locationManager = CLLocationManager() //colin
+
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        center.requestAuthorization(options: [.alert, .sound]) { granted, error in
+        }
+//        locationManager.requestAlwaysAuthorization()
+//        locationManager.startMonitoringVisits()
+//        locationManager.delegate = self as! CLLocationManagerDelegate
         FirebaseApp.configure()
         return true
     }
@@ -44,5 +56,40 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didVisit visit: CLVisit) {
+        // create CLLocation from the coordinates of CLVisit
+        let clLocation = CLLocation(latitude: visit.coordinate.latitude, longitude: visit.coordinate.longitude)
+
+        // Get location description
+        AppDelegate.geoCoder.reverseGeocodeLocation(clLocation) { placemarks, _ in
+            if let place = placemarks?.first {
+                let description = "\(place)"
+                self.newVisitReceived(visit, description: description)
+            }
+        }
+
+    }
+
+    func newVisitReceived(_ visit: CLVisit, description: String) {
+        let location = Location(visit: visit, descriptionString: description)
+        LocationsStorage.shared.saveLocationOnDisk(location)
+
+        // Save location to disk
+        // 1
+        let content = UNMutableNotificationContent()
+        content.title = "New Journal entry 📌"
+        content.body = location.description
+        content.sound = .default
+
+        // 2
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        let request = UNNotificationRequest(identifier: location.dateString, content: content, trigger: trigger)
+
+        // 3
+        center.add(request, withCompletionHandler: nil)
+    }
 }
 
